77f206c8a2318ac7e9931530099d735e3c5e978f
keoshayoung <keoshayoung@email.com>
2022-08-04 13:56:20 -0400

    adds search filter and autocomplete for search bar

--- src/views/PostsIndex.vue
+++ src/views/PostsIndex.vue
@@ -7,6 +7,8 @@ export default {
     return {
       message: "Blog Posts",
       posts: [],
+      currentPost: {},
+      titleFilter: "",
     };
   },
   created: function () {
@@ -22,13 +24,25 @@ export default {
     relativeDate: function (date) {
       return moment(date).fromNow();
     },
+    filterPosts: function () {
+      return this.posts.filter((post) => {
+        var lowerTitle = post.title.toLowerCase();
+        var lowerTitleFilter = this.titleFilter.toLowerCase();
+        return lowerTitle.includes(lowerTitleFilter);
+      });
+    },
   },
 };
 </script>
 
 <template>
   <h1>{{ message }}</h1>
-  <div v-for="post in posts" v-bind:key="post.id">
+  Search by title:
+  <input v-model="titleFilter" list="titles" type="text" />
+  <datalist id="titles">
+    <option v-for="post in posts" v-bind:key="post.id">{{ post.title }}</option>
+  </datalist>
+  <div v-for="post in filterPosts()" v-bind:key="post.id">
     <router-link v-bind:to="`/posts/${post.id}`">
       <h3>{{ post.title }}</h3>
     </router-link>
